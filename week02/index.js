447df6f876f1cca766939d00411e4337a8ad581b
chanmxim <chanmxim@gmail.com>
2025-09-16 18:31:26 -0400

    week2 ex66

--- week02/index.js
+++ week02/index.js
@@ -46,3 +46,33 @@ const calculateProduct = () => array.reduce((acc, number) => acc * number, 1);
 console.log("\nExercise 5:");
 console.log(calculateSum());
 console.log(calculateProduct());
+
+// Ex 6
+class Car{
+    constructor(model , year){
+        this.model = model;
+        this.year = year;
+    }
+
+    details(){
+        return `Model: ${this.model} Engine ${this.year}`;
+    }
+}
+
+class Sedan extends Car{
+    constructor(model, year, balance){
+        super(model, year);
+        this.balance = balance;
+    }
+
+    info(){
+        return `${this.model} has a balance of $${this.balance.toFixed(2)}`;
+    }
+}
+
+console.log("\nExercise 6:");
+const car2 = new Car('Pontiac Firebird', 1976);
+console.log(car2.details());
+
+const sedan = new Sedan('Volvo SD', 2018, 30000);
+console.log(sedan.info());
